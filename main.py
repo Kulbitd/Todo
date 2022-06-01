e4eace5531c8f3a9e9516b07b209a6e97af32496
kulbit <I>
2022-06-02 00:23:57 +0300

    Реализовали функцию создания туду листа

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import json
+
+
 def main():
     pass
+
+
+def create_todo_list(path_todo, todo_name):
+    with open(path_todo, "w") as todo_file:
+        json.dump(
+            {"name": todo_name},
+            todo_file,
+            sort_keys=True,
+            indent=4,
+        )
 
 
 if __name__ == '__main__':
