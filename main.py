dbb5aec07948a917c5e86a96c3278344eab5e59c
kulbit <I>
2022-06-02 00:58:35 +0300

    Написал докстринг для файла

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
+"""
+Создаем туду приложение
+"""
 import json
 import sys
 
 
 class TodoJournal:
+    """
+    Класс в котором определяем методы для работы с туду такие как удаление, удаление
+    """
+
     def __init__(self, path_todo, name):
         self.path_todo = path_todo
         self.name = name
 
     def create(self):
+        """
+        Функия создающая файл , в виде json, name - имя файла ,
+         todos[] - список , внутри которого будут наши to_do записки
+        :return: None
+        """
         with open(self.path_todo, "w", encoding='utf-8') as todo_file:
             json.dump(
                 {"name": self.name, "todos": []},
@@ -18,6 +30,11 @@ class TodoJournal:
             )
 
     def add_entry(self, new_entry):
+        """
+        Функция заполняющая наш список todos[] заметками
+        :param new_entry: Содержание заметки
+        :return: None
+        """
         data = self._parse()
 
         name = data["name"]
@@ -33,6 +50,11 @@ class TodoJournal:
         self._update(new_data)
 
     def remove_entry(self, index):
+        """
+        Функция удаления заметок в todos[] по индексу
+        :param index: Индекс заметки, которую хотим удалить
+        :return: None
+        """
         data = self._parse()
         name = data["name"]
         todos = data["todos"]
@@ -47,6 +69,11 @@ class TodoJournal:
         self._update(new_data)
 
     def _update(self, new_data):
+        """
+        Функция отвечающий за записи json в файл
+        :param new_data: Информация которую мы хотим записать
+        :return: None
+        """
         with open(self.path_todo, "w", encoding='utf-8') as todo_file:
             json.dump(
                 new_data,
@@ -57,6 +84,10 @@ class TodoJournal:
             )
 
     def _parse(self):
+        """
+        Функция , получающая содержимое нашего to_do файла
+        :return: data - содержимое to_do листа
+        """
         try:
             with open(self.path_todo, 'r') as todo_file:
                 data = json.load(todo_file)
@@ -68,6 +99,10 @@ class TodoJournal:
 
 
 def main():
+    """
+    Вызов и создание нашего журанала(по идее)
+    :return: None
+    """
     pass
 
 
