57f7d8070414820e3933018d5fb76bf438d0e64e
kulbit <I>
2022-06-02 03:08:33 +0300

    Добавил тест cо своей фикстурой

--- tests/test_todo_journal.py
+++ tests/test_todo_journal.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.TodoJournal import TodoJournal
 from src.TodoJournal import json
 
@@ -43,3 +45,41 @@ def test_add_entry(tmpdir):
         indent=4,
         ensure_ascii=False, )
     assert expected_todo == todo.read()
+
+
+@pytest.fixture()
+def todo_journal_with_3_entries(tmpdir):
+    todo_filename = "test_todo"
+    todo_path = tmpdir.join(todo_filename)
+    with open(todo_path, "w") as f:
+        json.dump(
+            {
+                "name": "test",
+                "todos": ["first entry", "second_entry", "third entry"]
+            },
+            f,
+            indent=4,
+            ensure_ascii=False, )
+    return todo_path
+
+
+@pytest.fixture()
+def todo_json_after_remove_second_entry():
+    return json.dumps(
+        {
+            "name": "test",
+            "todos": ["first entry", "third entry"]
+        },
+        indent=4,
+        ensure_ascii=False, )
+
+
+@pytest.fixture()
+def todo_object_with_with_3_entries(todo_journal_with_3_entries):
+    return TodoJournal(todo_journal_with_3_entries)
+
+
+def test_remove_entry(todo_object_with_with_3_entries, todo_json_after_remove_second_entry):
+    todo_object_with_with_3_entries.remove_entry(1)
+    expected_todo_json_after_remove_second_entry = todo_json_after_remove_second_entry
+    assert expected_todo_json_after_remove_second_entry == todo_object_with_with_3_entries.path_todo.read()
