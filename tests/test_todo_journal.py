3c21c09ed82715d5e764b181f6fd60460405fee0
kulbit <I>
2022-06-02 03:05:12 +0300

    Добавил тест добавление записи туду

--- tests/test_todo_journal.py
+++ tests/test_todo_journal.py
@@ -26,3 +26,20 @@ def test_create_journal(tmpdir):
         },
         indent=4)
     assert expected_todo == todo.read()
+
+
+def test_add_entry(tmpdir):
+    todo_filename = "test"
+    todo = tmpdir.join(todo_filename)
+    TodoJournal.create(todo, "test")
+    todo_jrnl = TodoJournal(todo)
+    todo_jrnl.add_entry("Сходить за молоком")
+
+    expected_todo = json.dumps(
+        {
+            "name": "test",
+            "todos": ["Сходить за молоком"]
+        },
+        indent=4,
+        ensure_ascii=False, )
+    assert expected_todo == todo.read()
