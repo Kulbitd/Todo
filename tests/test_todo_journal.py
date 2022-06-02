d3762ac57405c0fcbc98c45912084c1e134c92e8
kulbit <I>
2022-06-02 03:04:06 +0300

    Добавил тест ожидаемых данных

--- tests/test_todo_journal.py
+++ tests/test_todo_journal.py
@@ -1,4 +1,5 @@
 from src.TodoJournal import TodoJournal
+from src.TodoJournal import json
 
 
 def test_init(tmpdir):
@@ -11,3 +12,17 @@ def test_init(tmpdir):
     expected_name = "test_todo"
     assert entries == expected_entries
     assert name == expected_name
+
+
+def test_create_journal(tmpdir):
+    todo_filename = "test"
+    todo = tmpdir.join(todo_filename)
+    TodoJournal.create(todo, "test")
+
+    expected_todo = json.dumps(
+        {
+            "name": "test",
+            "todos": []
+        },
+        indent=4)
+    assert expected_todo == todo.read()
