617e8cc97475ff363f1ff8e19cc0d25d279cafc9
kulbit <I>
2022-06-04 17:02:34 +0300

    Решил переделать под click

--- src/todo.py
+++ src/todo.py
@@ -2,12 +2,14 @@ from src.TodoJournal import TodoJournal
 
 
 def run(args):
-    todo = TodoJournal("../tests/data/test_todo")
+    todo = TodoJournal("./test")
+    print("no")
     if args.text:
         todo.add_entry(args.text)
         raw_text = ''.join(args.text)
         todo.add_entry(raw_text)
     if args.delete:
+        print("yes")
         todo.remove_entry(int(args.delete))
     if args.path:
         with open("../config.config", "w") as file:
