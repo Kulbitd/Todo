29dada5721e3931e17a1a6fb87010ad0f9cd5725
kulbit <I>
2022-06-11 15:44:11 +0300

    Добавляю опции сценария cli

--- src/arg.py
+++ src/arg.py
@@ -1,2 +1,12 @@
+import click
 
 
+@click.command()  # Не может возвращать
+@click.option('text', help='Add new note', type=str)
+@click.option("--delete", "-d", "index", help='Enter the index what you want to remove', type=str)
+def parse_args(filename, index):
+    print(filename)
+
+
+if __name__ == "__main__":
+    parse_args()
