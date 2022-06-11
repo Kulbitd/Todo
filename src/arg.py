6bd52476f3a108477db20855da2c7e537b240d11
kulbit <I>
2022-06-11 16:55:09 +0300

    Добавляю функцию del с cli

--- src/arg.py
+++ src/arg.py
@@ -2,10 +2,11 @@ import click
 
 
 @click.command()  # Не может возвращать
-@click.option('text', help='Add new note', type=str)
-@click.option("--delete", "-d", "index", help='Enter the index what you want to remove', type=str)
-def parse_args(filename, index):
-    print(filename)
+@click.option('--text', prompt=True)
+@click.option("--delete", "-d", "index", type=str)
+def parse_args(text, index):
+    if index:
+        pass
 
 
 if __name__ == "__main__":
