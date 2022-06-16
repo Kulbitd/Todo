7b61ce280e1c5be697dac4adb5f5bd16df3588c0
kulbit <I>
2022-06-16 13:55:10 +0300

    С cklick не вышло, еще раз argparse

--- src/arg.py
+++ src/arg.py
@@ -1,11 +1,9 @@
-import click
+import argparse
 
 
-@click.command()  # Не может возвращать
-@click.option('--text', prompt=True)
-@click.option("--delete", "-d", "index", type=str)
-def parse_args(text, index):
-    if index:
-        return index
-    if text:
-        return text
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    composing = parser.add_argument_group("Writing new todos",
+                                          "чтобы добавить новую запись в туду лист необходимо выполнить Х")
+    composing.add_argument("text", metavar="", nargs="*")
+    return parser.parse_intermixed_args(args)
