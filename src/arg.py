3c8017386cbfdf8bf41174584ea798cd5816c863
kulbit <I>
2022-06-20 13:16:09 +0300

    Добавил удаление и запись

--- src/arg.py
+++ src/arg.py
@@ -3,7 +3,19 @@ import argparse
 
 def parse_args(args):
     parser = argparse.ArgumentParser()
+
     composing = parser.add_argument_group("Writing new todos",
                                           "чтобы добавить новую запись в туду лист необходимо выполнить Х")
     composing.add_argument("text", metavar="", nargs="*")
+
+    standalone = parser.add_argument_group(
+        "Standalone Commands",
+        "После выполнения этих команд работы программы завершиться",
+    )
+    standalone.add_argument(
+        "--delete",
+        dest="delete",
+        help="Удаляеть запись по индексу",
+    )
+
     return parser.parse_intermixed_args(args)
--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
+import sys
+
 from src.arg import parse_args
-from src.todo import run
 
 
 def main():
     try:
-        args = parse_args()
-        return run(args)
-    except Exception as e:
-        print(e)
-        return 1
+        # 1. Спарсить аргументы командной
+        cli_args = sys.argv[1:]
+        # 2. обработать аргументы командной строки
+        args = parse_args(cli_args)
+        print(args)
+    except:
+        pass
 
 
 if __name__ == '__main__':
