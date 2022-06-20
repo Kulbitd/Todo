027ec3b48100f190b6a0f34d14a15faea25e3982
kulbit <I>
2022-06-20 14:10:07 +0300

    Добавил аргументы к argparc-овским функциям

--- src/arg.py
+++ src/arg.py
@@ -2,11 +2,21 @@ import argparse
 
 
 def parse_args(args):
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     add_help=False,
+                                     description='----------------------------------------',
+                                     epilog='--------------------------------------')
+
+    # formatter_class - настройка форматирования справки
+    # description - краткое описание назначиния команды
+    # epilog - вывод текста снизу справки
 
     composing = parser.add_argument_group("Writing new todos",
                                           "чтобы добавить новую запись в туду лист необходимо выполнить Х")
     composing.add_argument("text", metavar="", nargs="*")
+    # metavar - изменяет имя атрибута в обьекте
+    # narg - кол во аргументов
+    # dest  - определяет имя атрибута
 
     standalone = parser.add_argument_group(
         "Standalone Commands",
