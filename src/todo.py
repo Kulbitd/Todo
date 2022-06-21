dbb8edfa3a1ac460b40c1da048a40a6a7140a930
kulbit <I>
2022-06-21 19:19:31 +0300

    функция записи path

--- src/todo.py
+++ src/todo.py
@@ -1,3 +1,5 @@
+import sys
+
 from src.TodoJournal import *
 
 
@@ -6,7 +8,7 @@ def run(args):
     if args.text:
         raw_text = ' '.join(args.text)
         todo.add_entry(raw_text)
-    if args.delete:
+    elif args.delete:
         todo.prin(int(args.delete))
         answer = input("Хотите удалить данную запись? [y/n]:    ")
         if answer == "yes" or answer == "y" or answer == "Yes" or answer == "Y":
@@ -16,3 +18,10 @@ def run(args):
             return
         else:
             print("Некоректный ввод ")
+    elif args.path:
+        path = args.path
+        try:
+            with open('../config.config', 'w') as f:
+                f.writelines(path)
+        except FileNotFoundError as e:
+            sys.exit(-0)
